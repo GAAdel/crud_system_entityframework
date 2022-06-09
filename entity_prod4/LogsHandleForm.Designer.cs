f520ab504647f4add453807335e8bd4500e602cb
CIKADA <cikada@gmail.com>
2022-06-09 15:44:41 +0300

    Вывод окна обработки ошибки и сохранение причины

--- entity_prod4/LogsHandleForm.Designer.cs
+++ entity_prod4/LogsHandleForm.Designer.cs
@@ -32,7 +32,7 @@
 			this.label1 = new System.Windows.Forms.Label();
 			this.radioBtnSoftCrash = new System.Windows.Forms.RadioButton();
 			this.btnConfirm = new System.Windows.Forms.Button();
-			this.textBoxSurname = new System.Windows.Forms.TextBox();
+			this.textBoxReason = new System.Windows.Forms.TextBox();
 			this.radioBtnSystCrash = new System.Windows.Forms.RadioButton();
 			this.SuspendLayout();
 			// 
@@ -42,9 +42,9 @@
 			this.labelCrashInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
 			this.labelCrashInfo.Location = new System.Drawing.Point(31, 44);
 			this.labelCrashInfo.Name = "labelCrashInfo";
-			this.labelCrashInfo.Size = new System.Drawing.Size(415, 29);
+			this.labelCrashInfo.Size = new System.Drawing.Size(439, 29);
 			this.labelCrashInfo.TabIndex = 17;
-			this.labelCrashInfo.Text = "Была обнаружена ошибка в работе";
+			this.labelCrashInfo.Text = "No logout detected for your last login on";
 			// 
 			// label1
 			// 
@@ -52,15 +52,15 @@
 			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
 			this.label1.Location = new System.Drawing.Point(31, 104);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(211, 29);
+			this.label1.Size = new System.Drawing.Size(102, 29);
 			this.label1.TabIndex = 18;
-			this.label1.Text = "Причина ошибки:";
+			this.label1.Text = "Reason:";
 			// 
 			// radioBtnSoftCrash
 			// 
 			this.radioBtnSoftCrash.AutoSize = true;
 			this.radioBtnSoftCrash.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.radioBtnSoftCrash.Location = new System.Drawing.Point(45, 430);
+			this.radioBtnSoftCrash.Location = new System.Drawing.Point(36, 574);
 			this.radioBtnSoftCrash.Name = "radioBtnSoftCrash";
 			this.radioBtnSoftCrash.Size = new System.Drawing.Size(229, 36);
 			this.radioBtnSoftCrash.TabIndex = 33;
@@ -72,27 +72,28 @@
 			// 
 			this.btnConfirm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.btnConfirm.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.btnConfirm.Location = new System.Drawing.Point(456, 508);
+			this.btnConfirm.Location = new System.Drawing.Point(599, 562);
 			this.btnConfirm.Name = "btnConfirm";
-			this.btnConfirm.Size = new System.Drawing.Size(331, 68);
+			this.btnConfirm.Size = new System.Drawing.Size(275, 61);
 			this.btnConfirm.TabIndex = 32;
-			this.btnConfirm.Text = "Сохранить";
+			this.btnConfirm.Text = "Confirm";
 			this.btnConfirm.UseVisualStyleBackColor = true;
+			this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
 			// 
-			// textBoxSurname
+			// textBoxReason
 			// 
-			this.textBoxSurname.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.textBoxSurname.Location = new System.Drawing.Point(36, 151);
-			this.textBoxSurname.Multiline = true;
-			this.textBoxSurname.Name = "textBoxSurname";
-			this.textBoxSurname.Size = new System.Drawing.Size(765, 210);
-			this.textBoxSurname.TabIndex = 31;
+			this.textBoxReason.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBoxReason.Location = new System.Drawing.Point(36, 155);
+			this.textBoxReason.Multiline = true;
+			this.textBoxReason.Name = "textBoxReason";
+			this.textBoxReason.Size = new System.Drawing.Size(838, 368);
+			this.textBoxReason.TabIndex = 31;
 			// 
 			// radioBtnSystCrash
 			// 
 			this.radioBtnSystCrash.AutoSize = true;
 			this.radioBtnSystCrash.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.radioBtnSystCrash.Location = new System.Drawing.Point(408, 430);
+			this.radioBtnSystCrash.Location = new System.Drawing.Point(326, 574);
 			this.radioBtnSystCrash.Name = "radioBtnSystCrash";
 			this.radioBtnSystCrash.Size = new System.Drawing.Size(211, 36);
 			this.radioBtnSystCrash.TabIndex = 34;
@@ -104,16 +105,16 @@
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(836, 606);
+			this.ClientSize = new System.Drawing.Size(911, 678);
 			this.Controls.Add(this.radioBtnSystCrash);
 			this.Controls.Add(this.radioBtnSoftCrash);
 			this.Controls.Add(this.btnConfirm);
-			this.Controls.Add(this.textBoxSurname);
+			this.Controls.Add(this.textBoxReason);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.labelCrashInfo);
 			this.Name = "LogsHandleForm";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-			this.Text = "Пользователь был ";
+			this.Text = "No logout detected";
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -125,7 +126,7 @@
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.RadioButton radioBtnSoftCrash;
 		private System.Windows.Forms.Button btnConfirm;
-		private System.Windows.Forms.TextBox textBoxSurname;
+		private System.Windows.Forms.TextBox textBoxReason;
 		private System.Windows.Forms.RadioButton radioBtnSystCrash;
 	}
 }
--- entity_prod4/LogsHandleForm.cs
+++ entity_prod4/LogsHandleForm.cs
@@ -12,10 +12,51 @@ namespace entity_prod4
 {
 	public partial class LogsHandleForm : Form
 	{
+		public int lastLogCrashId;
 		public LogsHandleForm(int logCrashID)
 		{
 			InitializeComponent();
-			
+
+			lastLogCrashId = logCrashID;
+
+			MyDbContext db = new MyDbContext();
+			var dbLogs = db.Logs;
+
+			var lastCrash = dbLogs.FirstOrDefault(log => log.ID == lastLogCrashId);
+
+			labelCrashInfo.Text = "No logout detected for your last login on " + lastCrash.Date + " at " + lastCrash.LoginTime;
 		}
+
+		private void btnConfirm_Click(object sender, EventArgs e)
+		{
+			MyDbContext db = new MyDbContext();
+			var dbLogs = db.Logs;
+
+			var lastCrash = dbLogs.FirstOrDefault(log => log.ID == lastLogCrashId);
+
+			if (radioBtnSoftCrash.Checked)
+			{
+				lastCrash.Reason = "Software Crash";
+				db.SaveChanges();
+				this.Close();
+			}
+			else if (radioBtnSystCrash.Checked)
+			{
+				lastCrash.Reason = "System Crash";
+				db.SaveChanges();
+				this.Close();
+			}
+			else if (textBoxReason.Text != "")
+			{
+				lastCrash.Reason = textBoxReason.Text;
+				db.SaveChanges();
+				this.Close();
+			}
+			else
+			{
+				MessageBox.Show("Введите причину ошибки в поле или выберите ошибку.");
+			}
+		}
+
 	}
 }
